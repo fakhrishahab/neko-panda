2eaf80d76768067876e2d42dbd39d5e85b0cba0d
Akhmad Fakhri <akhmad.fakhri@metraplasa.co.id>
2018-11-27 13:15:14 +0700

    add header

--- header.js
+++ header.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     var url = document.location.href.split("/");
     var path = url[url.length - 1];
+    console.log(path);
     
     $('.header-nav-item[data-index="'+path+'"] a').addClass('active');
 });
